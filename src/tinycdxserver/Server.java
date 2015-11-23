affedede396c63bb6b42defe341c461eb8af1102
Alex Osborne <aosborne@nla.gov.au>
2015-11-24 10:19:45 +1100

    Basic ugly admin ui

--- src/tinycdxserver/Server.java
+++ src/tinycdxserver/Server.java
@@ -1,47 +1,42 @@
 package tinycdxserver;
 
 
-import org.archive.accesscontrol.AccessControlClient;
-import org.archive.accesscontrol.RobotsUnavailableException;
-import org.archive.accesscontrol.RuleOracleUnavailableException;
+import com.google.gson.Gson;
 import org.rocksdb.RocksDB;
 import org.rocksdb.RocksDBException;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.URL;
-import java.nio.channels.Channel;
-import java.nio.channels.ServerSocketChannel;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import static tinycdxserver.NanoHTTPD.Method.GET;
 import static tinycdxserver.NanoHTTPD.Method.POST;
 import static tinycdxserver.NanoHTTPD.Response.Status.*;
 
 public class Server extends NanoHTTPD {
-    private final DataStore manager;
+    private static final Gson gson = new Gson();
+    private final DataStore dataStore;
     boolean verbose = false;
 
-    public Server(DataStore manager, String hostname, int port) {
+    public Server(DataStore dataStore, String hostname, int port) {
         super(hostname, port);
-        this.manager = manager;
+        this.dataStore = dataStore;
     }
 
-    public Server(DataStore manager, ServerSocket socket) {
+    public Server(DataStore dataStore, ServerSocket socket) {
         super(socket);
-        this.manager = manager;
+        this.dataStore = dataStore;
     }
 
     String extension(String filename) {
         int i = filename.lastIndexOf('.');
-        if (i != -1) {
-            return filename.substring(i + 1);
-        } else {
-            return null;
-        }
+        return i != -1 ? filename.substring(i + 1) : null;
     }
 
     String guessContentType(String filename) {
@@ -54,13 +49,31 @@ public class Server extends NanoHTTPD {
         }
     }
 
-    InputStream openResource(String filename) {
-        return getClass().getResourceAsStream(getClass().getPackage() + "/" + filename);
+    Response serveResource(String uri) throws IOException {
+        if (uri.contains("..")) {
+            return notFound();
+        }
+
+        URL resource = getClass().getResource(uri.substring(1));
+        if (resource == null) {
+            return notFound();
+        }
+
+        Response response = new Response(OK, guessContentType(uri), resource.openStream());
+        //response.addHeader("Cache-Control", "max-age=31536000");
+        return response;
     }
 
     Response notFound() {
         return new Response(NOT_FOUND, "text/plain", "Not found\n");
     }
+
+    Response jsonResponse(Object data) {
+        return new Response(OK, "application/json", gson.toJson(data));
+    }
+
+
+    private static final Pattern CAPTURES_ROUTE = Pattern.compile("/api/collections/(" + DataStore.COLLECTION_PATTERN + ")/captures");
 
     @Override
     public Response serve(IHTTPSession session) {
@@ -69,16 +82,33 @@ public class Server extends NanoHTTPD {
             Method method = session.getMethod();
             if (uri.equals("/")) {
                 return collectionList();
-            } else if (method.equals(GET) && uri.startsWith("/static/") && !uri.contains("..")) {
-                URL resource = getClass().getResource(uri.substring(1));
-                if (resource != null) {
-                    Response response = new Response(OK, guessContentType(uri), resource.openStream());
-                    response.addHeader("Cache-Control", "max-age=31536000");
-                    return response;
-                }
-            } else if (method.equals(GET)) {
+            }
+            if (uri.startsWith("/admin")) {
+                return serveResource("/static/main.html");
+            }
+            if (method == GET && uri.equals("/api/collections")) {
+                return jsonResponse(dataStore.listCollections());
+            }
+
+            Matcher m = CAPTURES_ROUTE.matcher(uri);
+            if (method == GET && m.matches()) {
+                String collection = m.group(1);
+                String key = session.getParms().getOrDefault("key", "");
+                long limit = Long.parseLong(session.getParms().getOrDefault("limit", "1000"));
+                Index index = dataStore.getIndex(collection);
+                List<Capture> results = StreamSupport.stream(index.listCaptures(key).spliterator(), false)
+                        .limit(limit)
+                        .collect(Collectors.<Capture>toList());
+                return jsonResponse(results);
+            }
+
+            if (method == GET && uri.startsWith("/static/")) {
+                return serveResource(uri);
+            }
+            if (method == GET) {
                 return query(session);
-            } else if (method.equals(POST)) {
+            }
+            if (method == POST) {
                 return post(session);
             }
             return notFound();
@@ -90,7 +120,7 @@ public class Server extends NanoHTTPD {
 
     Response post(IHTTPSession session) throws IOException {
         String collection = session.getUri().substring(1);
-        final Index index = manager.getIndex(collection, true);
+        final Index index = dataStore.getIndex(collection, true);
         BufferedReader in = new BufferedReader(new InputStreamReader(session.getInputStream()));
         long added = 0;
 
@@ -125,7 +155,7 @@ public class Server extends NanoHTTPD {
 
     Response query(IHTTPSession session) throws IOException {
         String collection = session.getUri().substring(1);
-        final Index index = manager.getIndex(collection);
+        final Index index = dataStore.getIndex(collection);
         if (index == null) {
             return new Response(NOT_FOUND, "text/plain", "Collection does not exist\n");
         }
@@ -157,13 +187,13 @@ public class Server extends NanoHTTPD {
     private Response collectionList() throws IOException {
         String page = "<!doctype html><h1>tinycdxserver</h1>";
 
-        List<String> collections = manager.listCollections();
+        List<String> collections = dataStore.listCollections();
 
         if (collections.isEmpty()) {
             page += "No collections.";
         } else {
             page += "<ul>";
-            for (String collection : manager.listCollections()) {
+            for (String collection : dataStore.listCollections()) {
                 page += "<li><a href=" + collection + ">" + collection + "</a>";
             }
             page += "</ul>";
@@ -196,80 +226,4 @@ public class Server extends NanoHTTPD {
         });
     }
 
-    public static void usage() {
-        System.err.println("Usage: java " + Server.class.getName() + " [options...]");
-        System.err.println("");
-        System.err.println("  -a url        Use a wayback access control oracle");
-        System.err.println("  -b bindaddr   Bind to a particular IP address");
-        System.err.println("  -d datadir    Directory to store index data under");
-        System.err.println("  -i            Inherit the server socket via STDIN (for use with systemd, inetd etc)");
-        System.err.println("  -p port       Local port to listen on");
-        System.err.println("  -v            Verbose logging");
-        System.exit(1);
-    }
-
-    public static void main(String args[]) {
-        String host = null;
-        int port = 8080;
-        boolean inheritSocket = false;
-        File dataPath = new File("data");
-        boolean verbose = false;
-        Predicate<Capture> filter = null;
-
-        for (int i = 0; i < args.length; i++) {
-            switch (args[i]) {
-                case "-a":
-                    filter = accessControlFilter(args[++i]);
-                    break;
-                case "-p":
-                    port = Integer.parseInt(args[++i]);
-                    break;
-                case "-b":
-                    host = args[++i];
-                    break;
-                case "-i":
-                    inheritSocket = true;
-                    break;
-                case "-d":
-                    dataPath = new File(args[++i]);
-                    break;
-                case "-v":
-                    verbose = true;
-                    break;
-                default:
-                    usage();
-                    break;
-            }
-        }
-
-        try (DataStore dataStore = new DataStore(dataPath, filter)) {
-            final Server server;
-            Channel channel = System.inheritedChannel();
-            if (inheritSocket && channel != null && channel instanceof ServerSocketChannel) {
-                server = new Server(dataStore, ((ServerSocketChannel) channel).socket());
-            } else {
-                server = new Server(dataStore, host, port);
-            }
-            server.verbose = verbose;
-            server.start();
-            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-                server.stop();
-                dataStore.close();
-            }));
-            Thread.sleep(Long.MAX_VALUE);
-        } catch (InterruptedException | IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static Predicate<Capture> accessControlFilter(String oracleUrl) {
-        AccessControlClient client = new AccessControlClient(oracleUrl);
-        return capture -> {
-            try {
-                return "allow".equals(client.getPolicy(capture.original, new Date(capture.timestamp), new Date(), "public"));
-            } catch (RobotsUnavailableException | RuleOracleUnavailableException e) {
-                throw new RuntimeException(e);
-            }
-        };
-    }
 }
